0f373be201deb38d7c01f460b167960e8c3ed0b1
akazademonr <ajibefunemmanuel5@gmail.com>
2026-06-13 12:16:37 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import { SmoothScrollProvider } from './providers/SmoothScrollProvider.tsx'
-import ScrollToTop from './components/ScrollToTop.tsx'
+import { SmoothScrollProvider } from './providers/SmoothScrollProvider'
+import ScrollToTop from './components/ScrollToTop'
 
 if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual';
